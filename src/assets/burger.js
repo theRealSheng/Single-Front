8e5b8a0f367b72c4a065db32bf47b2b8f796e996
Jana Stinn <jana-stinn@gmx.de>
2018-03-05 11:06:28 +0100

    started burger menu

---
+++ src/assets/burger.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.getElementById("img-wrapper").addEventListener ("click", toggleNav);
+
+function toggleNav() {
+  var nav = document.getElementById("main-nav");
+  var className = nav.getAttribute("class");
+  if(className == "nav-right nav-menu") {
+      nav.className = "nav-right nav-menu active";
+  } else {
+      nav.className = "nav-right nav-menu";
+  }
+}
